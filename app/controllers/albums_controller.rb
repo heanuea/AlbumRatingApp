71aef7785f5fe00a62b6cbf01e4968f74a4bb6cc
Alan Heanue <g00318763@gmit.ie>
2017-02-25 14:18:12 +0000

    Added view files

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,37 @@
 class AlbumsController < ApplicationController
+	before_action :find_album, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@albums = Album.all.order("created_at DESC")
 	end
+
+	def show
+	end
+
 
 	def new	
 		@album = Album.new
 	end
 
+
 	def create	
+		@album = Album.new(album_params)
+
+		if @album.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
+
+	private
+
+	def album_params
+		params.require(:album).permit(:title, :info, :artist)	
+	end
+
+	def find_album	
+		@album = Album.find(params[:id])
+	end
 end
