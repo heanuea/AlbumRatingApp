7e676fa44727d137292e5287dd2e1fa0c5f87428
Alan Heanue <g00318763@gmit.ie>
2017-03-02 13:58:37 +0000

    Added user and welcome screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #This resources creates all the routes for the crud functionality
 	resources :albums 
 	root 'albums#index'
 	
